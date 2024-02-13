60eb31024ba46c6581d1b18e0ba13ba706579d09
VasiliyZhel <Tars.tarson@gmail.com>
2024-02-13 22:31:53 +0300

    Аннотирован файл Chips.py

--- Chips.py
+++ Chips.py
@@ -1,14 +1,15 @@
 def get_chips_count():
+    """Функция рассчитывает количество чипсов,
+    которое нужно съесть, чтоб набрать требуемое
+    количество каллорий"""
     s = int(input("Сколько каллорий вы хотите набрать?: "))
-    kkal_to100 = 510
-    weight_one = 140
-    kkal_in_one = kkal_to100 * weight_one // 100
-    one_gramm_kkal = kkal_in_one / weight_one
-    itog = s / one_gramm_kkal
-    part_of_box = itog / weight_one
+    kkal_to100 = 510 # Каллорий в ста граммах продукта
+    weight_one = 140 # Вес пачки
+    kkal_in_one = kkal_to100 * weight_one // 100 # Каллорий в пачке
+    one_gramm_kkal = kkal_in_one / weight_one # Каллорий в грамме
+    itog = s / one_gramm_kkal # Итоговое количество грамм
+    part_of_box = itog / weight_one # Количество пачек
     print(f"Вам необходимо съесть {itog.__round__(2)} грамм, что равняется {part_of_box.__round__(2)} Пачки ")
-
-#get_chips_count()
 
 s = ""
 while s != "нет":
